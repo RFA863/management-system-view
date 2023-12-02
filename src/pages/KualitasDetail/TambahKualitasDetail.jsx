f2ce41413f886ad8f799a5278d31ec1dffbb3f6f
firmansyah.bandung11 <firmansyah.bandung11@gmail.com>
2023-12-02 17:42:01 +0700

    Bebas terserah

---
+++ src/pages/KualitasDetail/TambahKualitasDetail.jsx
@@ -0,0 +1,207 @@
+import axios from "axios";
+import { getCookie } from "cookies-next";
+import { CgClose } from "react-icons/cg";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+
+import { HOST } from "../../config";
+import { Header } from "../../components";
+
+import "react-toastify/dist/ReactToastify.css";
+
+const TambahKualitasDetail = () => {
+  const navigate = useNavigate();
+
+  const [nama, setNama] = useState("");
+  const [kode, setKode] = useState("");
+  const [Id_Kualitas, setId_Kualitas] = useState("");
+
+
+  const Validator = () => {
+    
+
+    if (
+        !(
+          nama &&
+          kode &&
+          Id_Kualitas 
+        )
+      ) {
+      toast.error("Data must be entered", {
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "colored",
+      });
+
+      return false;
+    } 
+
+    return true;
+  };
+
+  const postData = async (e) => {
+    e.preventDefault();
+
+    if (!Validator()) {
+      return;
+    }
+    await axios
+      .post(
+        HOST + "/marketing/kualitasdetail/input",
+        {
+          nama,
+          kode,
+          id_kualitas:Number(Id_Kualitas)
+
+        },
+        {
+          headers: {
+            "ngrok-skip-browser-warning": true,
+            Authorization: getCookie("admin_auth"),
+          },
+        }
+      )
+      .then((response) => {
+        if (response.status === 200) {
+          toast.success("Data successfully inputted", {
+            position: "top-center",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "colored",
+          });
+        }
+      })
+      .catch((error) => {
+        if (error.response) {
+          // console.log(error.response.data.type);
+          if (
+            error.response.data.type === "token" &&
+            error.response.data.data.code === -2
+          ) {
+            navigate("/dashboard/login");
+          }
+          toast.error(error.response.data.message, {
+            position: "top-center",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "colored",
+          });
+        } else {
+          toast.error("Internal Server Error, Try again later !", {
+            position: "top-center",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "colored",
+          });
+        }
+      });
+  };
+
+  return (
+    <div>
+      <div className="m-2 md:m-10 mt-24 px-2 py-10 md:p-10 bg-white rounded-3xl ">
+        <div className="flex justify-between">
+          <Header title="Tambah Kualitas Detail" />
+          <CgClose
+            className="text-4xl cursor-pointer"
+            onClick={() => {
+              navigate("/dashboard/master/kualitas-detail");
+            }}
+          />
+        </div>
+        <form>
+          <div className="flex items-end justify-evenly">
+            <table className="font-semibold">
+              
+              <tr>
+                <td>Nama</td>
+                <td>:</td>
+                <td>
+                  <input
+                    type="text"
+                    className="border-b-2 focus:outline-none focus:border-blue-700 w-[300px] "
+                    value={nama}
+                    onChange={(e) => {
+                      setNama(e.target.value);
+                    }}
+                    required
+                  />
+                </td>
+              </tr>
+              <tr>
+                <td>Kode</td>
+                <td>:</td>
+                <td>
+                  <input
+                    type="text"
+                    className="border-b-2 focus:outline-none focus:border-blue-700 w-[300px] "
+                    value={kode}
+                    onChange={(e) => {
+                      setKode(e.target.value);
+                    }}
+                    required
+                  />
+                </td>
+              </tr>
+              <tr>
+                <td>Id Kualitas</td>
+                <td>:</td>
+                <td>
+                  <input
+                    type="text"
+                    className="border-b-2 focus:outline-none focus:border-blue-700 w-[300px] "
+                    value={Id_Kualitas}
+                    onChange={(e) => {
+                      setId_Kualitas(e.target.value);
+                    }}
+                    required
+                  />
+                </td>
+              </tr>
+             
+            </table>
+            <div>
+              <button
+                className="bg-blue-700 rounded-xl text-white px-4 py-2"
+                onClick={postData}
+              >
+                Submit
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+      <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+      />
+    </div>
+  );
+};
+export default TambahKualitasDetail;
