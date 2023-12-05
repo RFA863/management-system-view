1c448524e7d735708d2bb8852430a424743ae538
Frahari7 <frahariputra@upi.edu>
2023-12-05 23:02:10 +0700

    fit : index

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -13,6 +13,9 @@ import {
   TypeboxDetail,
   TambahTypeboxDetail,
   UpdateTypeboxDetail,
+  IndexHarga,
+  TambahIndex,
+  UpdateIndex,  
 } from "../pages";
 
 import {
@@ -95,14 +98,34 @@ const Main = () => {
               {/* Pelamar  */}
               <Route path="/master/type-box" element={<Typebox />} />
               <Route path="/TypeBox/BuatTypebox" element={<BuatTypebox />} />
-              <Route path="/TypeBox/UpdateTypebox" element={<UpdateTypebox />} />
+              <Route
+                path="/TypeBox/UpdateTypebox"
+                element={<UpdateTypebox />}
+              />
               <Route path="/customer/customers" element={<Customer />} />
               <Route path="/customer/tambah" element={<TambahCustomer />} />
               <Route path="/customer/update" element={<UpdateCustomer />} />
-              <Route path="/master/type-box%20detail" element={<TypeboxDetail />} />
-              <Route path="/TypeboxDetail/Update" element={<UpdateTypeboxDetail />} />
-              <Route path="/TypeboxDetail/Buat" element={<TambahTypeboxDetail />} />  
-
+              <Route
+                path="/master/type-box%20detail"
+                element={<TypeboxDetail />}
+              />
+              <Route
+                path="/TypeboxDetail/Update"
+                element={<UpdateTypeboxDetail />}
+              />
+              <Route
+                path="/TypeboxDetail/Buat"
+                element={<TambahTypeboxDetail />}
+              />
+              <Route path="/index/index" element={<IndexHarga />} />
+              <Route
+                path="/TambahIndex/TambahIndex"
+                element={<TambahIndex />}
+              />
+              <Route
+                path="/UpdateIndex/UpdateIndex"
+                element={<UpdateIndex />}
+              />
             </Routes>
           </div>
           <Footer />
